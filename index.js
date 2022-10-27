d5114868839b86a573743e15bcd35c7caba80b04
MD Arif <p2b.business.info@gmail.com>
2022-10-27 23:53:33 +0600

    added subjects id

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ app.get('/', (req, res) => {
 
 app.get('/subjectLists', (req, res) => {
     res.send(subjectsList)
-}) 
-  
+})  
 app.get('/detailsSubs/:id', (req, res) => {
     const id = req.params.id
     const detailSub = subjectDetails.filter(sd => sd.id === id)
